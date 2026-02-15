b5ce6a3de708dd3c28536daf1a18e94ea868ae87
Alex <rayfloresd@gmail.com>
2026-02-14 19:07:40 -0500

    Day 2 main.py
    Added pipelines (functions) to the code

--- project_1_crypto_pipeline/main.py
+++ project_1_crypto_pipeline/main.py
@@ -1,33 +1,56 @@
 import requests
 import pandas as pd
 
-# Step 1: Extract data from CoinGecko API
-url = "https://api.coingecko.com/api/v3/coins/markets"
-params = {
-    "vs_currency": "usd",
-    "order": "market_cap_desc",
-    "per_page": 10,
-    "page": 1,
-    "sparkline": False
-}
+def extract_data():
+    url = "https://api.coingecko.com/api/v3/coins/markets"
+    params = {
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": 10,
+        "page": 1,
+        "sparkline": False
+    }
 
-response = requests.get(url, params=params)
-data = response.json()
+    response = requests.get(url, params=params)
+    if response.status_code != 200:
+        print("Error fetching data")
+        return None
 
-# Step 2: Convert to DataFrame
-df = pd.DataFrame(data)
+    return response.json()
 
-# Step 3: Select relevant columns
-df = df[[
-    "id",
-    "symbol",
-    "name",
-    "current_price",
-    "market_cap",
-    "total_volume"
-]]
+def transform_data(data):
+    if not data:
+    print("No data to transform.")
+    return None
 
-# Step 4: Save to CSV
-df.to_csv("data/crypto_data.csv", index=False)
+    df = pd.DataFrame(data)
 
-print("Data pipeline executed successfully.")
+    if df.empty:
+        print("DataFrame is empty.")
+        return None
+    
+    df = df[[
+        "id",
+        "symbol",
+        "name",
+        "current_price",
+        "market_cap",
+        "total_volume"
+    ]]
+    return df
+
+def load_data(df):
+    if df is None:
+    print("No data to load.")
+    return
+
+    df.to_csv("data/crypto_data.csv", index=False)
+    print("Data saved successfully.")
+
+def main():
+    data = extract_data()
+    df = transform_data(data)
+    load_data(df)
+
+if __name__ == "__main__":
+    main()
